d76a3172859defe1195905ddfca543f43639f7eb
Vũ Thanh Thiên <thien.vu2@sotatek.com>
2021-12-14 16:39:42 +0700

    update variable env

--- orderapp/src/order/order.controller.spec.ts
+++ orderapp/src/order/order.controller.spec.ts
@@ -1,81 +1,35 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { OrderController } from './order.controller';
-import { OrderService } from './order.service'
-import { OrderHistoryService } from './order-history.service'
-import { OrderEntity, Status } from './order.entity';
-import {CreateOrderDto} from './dto/create-order.dto'
-describe('Order Controller', () => {
-    let controller: OrderController;
-    let orderService: OrderService;
-    let orderHistoryService : OrderHistoryService;
+import { OrderService } from './order.service';
+import { OrderHistoryService } from './order-history.service';
+import { OrderEntity } from './order.entity';
+import { Test } from '@nestjs/testing';
+describe('OrderController', () => {
+  let orderController: OrderController;
+  let orderService: OrderService;
+  let orderHistoryService: OrderHistoryService;
 
-    beforeEach(async () => {
-        const ApiServiceProvider = {
-            provide: OrderService,
-            useFactory: () => ({
-                findAll: jest.fn(() => []),
-            }),
-        };
-        const app: TestingModule = await Test.createTestingModule({
-            controllers: [OrderController],
-            providers: [OrderService, ApiServiceProvider],
-        }).compile();
-        controller = app.get<OrderController>(OrderController);
-        orderService = app.get<OrderService>(OrderService);
-        orderHistoryService = app.get<OrderHistoryService>(OrderHistoryService);
-    });
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+        controllers: [OrderController],
+        providers: [OrderService,OrderHistoryService ],
+      }).compile();
+      orderService = moduleRef.get<OrderService>(OrderService);
+      orderHistoryService = moduleRef.get<OrderHistoryService>(OrderHistoryService);
+      orderController = moduleRef.get<OrderController>(OrderController);
+  });
 
-    it('should be defined', () => {
-        expect(controller).toBeDefined();
+  describe('findAll', () => {
+    it('should return list of order', async (done) => {
+        const userID = "12";
+        let result = orderService.findAll(userID);
+        result
+          .then(rslt => expect(rslt).toBe('foo'))
+          .then(done);
+    //   const result =  ;
+    //   const userID = "12";
+    //   jest.spyOn(orderService, 'findAll').mockImplementation(() => result);
+
+    //   expect(await orderController.findAll(userID)).toBe(result);
     });
-    /**findAll */
-    describe('findAll', () => {
-        it('should return an array of order', async () => {
-            controller.findAll();
-            expect(orderService.findAll()).toHaveBeenCalled();
-        });
-    });
-    /**findOne */
-    describe('findOne', () => {
-        it('should return an array of order', async (id) => {
-            const temp = new OrderEntity;
-            temp.id = 1
-            temp.orderNumber = "WtGb6Eqs"
-            temp.name ="Bánh"
-            temp.category = "Đồ ăn"
-            temp.image = "https://cf.shopee.vn/file/09ca6f8e32962f7cda47d8020b7b7162"
-            temp.address = "Hoàng Hoa Thám"
-            temp.description = "Bánh hảo hảo"
-            temp.price = 23000
-            temp.quantity = 10
-            temp.status = Status.CANCELLED
-            temp.createTimestamp = new Date()
-            temp.updateTimestamp = new Date()
-            controller.findOne(id);
-            expect(orderService.findOne(id)).toBe(temp);
-        });
-    });
-    /**find detail history */
-    describe('findDetail', () => {
-        it('should return an array of order', async (orderNumber) => {
-            controller.findDetail(orderNumber);
-            expect(orderHistoryService.findByOrderNumber(orderNumber)).toHaveBeenCalled();
-        });
-    });
-    /**create order */
-    describe('create', () => {
-        it('should return an array of order', async () => {
-            const dtoTemp = {
-                "name": "Bánh xe",
-                "price": 109.5,
-                "address": "Hà Nội",
-                "quantity": 1,
-                "description": "Hàng dởm",
-                "category": "thời trang nam",
-                "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
-            };
-            controller.create(dtoTemp);
-            expect(orderService.create(dtoTemp)).toHaveBeenCalled();
-        });
-    });
+  });
 });
