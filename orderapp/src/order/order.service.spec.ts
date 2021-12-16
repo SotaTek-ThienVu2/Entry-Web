f76b70c3e7f036a9af801c8dfee5dd0e370e99eb
Vũ Thanh Thiên <thien.vu2@sotatek.com>
2021-12-16 10:16:38 +0700

    update unit test

--- orderapp/src/order/order.service.spec.ts
+++ orderapp/src/order/order.service.spec.ts
@@ -8,11 +8,15 @@ import { HttpService } from '@nestjs/axios';
 import { Order } from './order.entity';
 import { OrderMockData, OrderListMockData, CreateOrderMockDto} from './mock/mockdata';
 import { Status } from '../common/enum/Status';
+import {
+  HttpStatus,
+  HttpException
+} from '@nestjs/common';
 describe('OrderService', () => {
   let service: OrderService;
   let orderHistoryService: OrderHistoryService;
 
-  const mockOrderHistoryService = {
+  let mockOrderHistoryService = {
     insert: jest.fn().mockImplementation((status: Status, orderNumber: string)=>{
       Promise.resolve({
         id: 9,
@@ -22,7 +26,7 @@ describe('OrderService', () => {
       })
     })
   };
-  const mockOrderRepository = {
+  let mockOrderRepository = {
     findAll: jest.fn().mockImplementation((userID: string) =>
       Promise.resolve(OrderListMockData),
     ),
@@ -47,7 +51,8 @@ describe('OrderService', () => {
     }),
     update: jest.fn().mockImplementation((id, order)=>{
       Promise.resolve(order)
-    })
+    }),
+    delete: jest.fn().mockImplementation((id: number) => Promise.resolve({affected: 1}))
   };
 
   beforeEach(async () => {
@@ -87,16 +92,26 @@ describe('OrderService', () => {
     expect(await service.findOne(9)).toEqual(OrderMockData);
   });
 
-  // it(`should get the created order`, async () => {
-  //   await mockOrderRepository.insert(OrderMockData)
-  //   await mockOrderHistoryService.insert({
-  //     id: 9,
-  //     orderNumber: "5x5sjP3m",
-  //     status: Status.CREATED,
-  //     createTimestamp: "2021-12-15T10:01:01.000Z"})
-  //   expect( service.create( CreateOrderMockDto , "12")).resolves.toEqual(OrderMockData);
-  //   expect( service.create( CreateOrderMockDto , "12")).rejects.toBeCalled();
-  // });
+  it(`should get the created order`, async () => {
+    mockOrderRepository = {
+      ...mockOrderRepository,
+      insert: jest.fn().mockResolvedValue(OrderMockData),
+    };
+    mockOrderHistoryService = {
+      insert: jest.fn().mockResolvedValue({
+        id: 9,
+        orderNumber: "5x5sjP3m",
+        status: Status.CREATED,
+        createTimestamp: "2021-12-15T10:01:01.000Z"
+      })
+    }
+    try {
+      const res = service.create(CreateOrderMockDto, "12")
+      expect(res).toEqual(OrderMockData);
+    } catch (err) {
+      new HttpException( err , HttpStatus.BAD_REQUEST)
+    }
+  });
 
   it(`should update order with id`, async () => {
     await mockOrderRepository.update( 9, OrderMockData)
@@ -122,6 +137,53 @@ describe('OrderService', () => {
       updateTimestamp: new Date('2021-12-15T10:11:54.000Z')
     })
     expect(await service.findOne(9)).toEqual(OrderMockData);
+  });
+
+  it(`should cancel order with id`, async () => {
+    const order = await service.findOne(9)
+    mockOrderHistoryService.insert( order.orderNumber , Status.CANCELLED)
+    await service.update({
+      id: 9,
+      orderNumber: '5x5sjP3m',
+      userID: '12',
+      name: 'Bánh tráng nướng',
+      category: 'Đồ ăn',
+      image: 'https://yummyday.vn/uploads/images/cach-lam-banh-trang-nuong-6.jpg',
+      address: 'Nhà ăn sinh viên Bách Khoa',
+      description: 'Ăn ngon thì vl',
+      price: 25000,
+      quantity: 15,
+      status: Status.CANCELLED,
+      createTimestamp: new Date('2021-12-15T10:01:01.000Z'),
+      updateTimestamp: new Date('2021-12-15T10:11:54.000Z')
+    })
+    expect(await service.findOne(9)).toEqual(OrderMockData);
+  });
+
+  it(`should confirm order with id`, async () => {
+    const order = await service.findOne(9)
+    mockOrderHistoryService.insert( order.orderNumber , Status.CONFIRMED)
+    await service.update({
+      id: 9,
+      orderNumber: '5x5sjP3m',
+      userID: '12',
+      name: 'Bánh tráng nướng',
+      category: 'Đồ ăn',
+      image: 'https://yummyday.vn/uploads/images/cach-lam-banh-trang-nuong-6.jpg',
+      address: 'Nhà ăn sinh viên Bách Khoa',
+      description: 'Ăn ngon thì vl',
+      price: 25000,
+      quantity: 15,
+      status: Status.CONFIRMED,
+      createTimestamp: new Date('2021-12-15T10:01:01.000Z'),
+      updateTimestamp: new Date('2021-12-15T10:11:54.000Z')
+    })
+    expect(await service.findOne(9)).toEqual(OrderMockData);
+  });
+
+  it(`should delete order with id`, async () => {
+    await mockOrderRepository.delete(9)
+    expect(await service.delete(9)).toEqual({affected: 1});
   });
 
 });
